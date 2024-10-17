dfc5a7860a759be0c38f873b728a98f070e3d797
varshan ms <msvarshan07@gmail.com>
2024-10-17 14:43:44 +0530

    Tabs style classes (#37)

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,22 @@ import { defineConfig } from 'vite'
 import browserslist from 'browserslist';
 import {browserslistToTargets} from 'lightningcss';
 import { bundle } from 'lightningcss';
+import path from "path";
+import {Plugin} from "./plugin-script";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [Plugin()],
   css: {
     //transformer: 'lightningcss',
     transform: "postCss",
     lightningcss: {
       targets: browserslistToTargets(browserslist('>= 0.25%'))
+    }
+  },
+  resolve: {
+    alias: {
+      "@aalam-wc": path.resolve(__dirname, "./node_modules/aalam-wc/")
     }
   },
   build: {
